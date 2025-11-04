b69c154987265688c2675256ab003bc6cca57a36
Ilesanmi Tolulope <marvellousilesanmi25@gmail.com>
2025-11-04 21:52:23 +0100

    fixing checkout issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  env: {
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+  },
 };
 
 export default nextConfig;
